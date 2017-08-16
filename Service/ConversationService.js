783539b2e48271be008b70416f01fb94d5997b2a
giseoplee <llgs901@naver.com>
2017-08-16 14:06:39 +0900

    1
    1

--- Service/ConversationService.js
+++ Service/ConversationService.js
@@ -5,37 +5,30 @@ var Conversation = require('watson-developer-cloud/conversation/v1');
 var config = require('../Common/Config.js');
 var redis = require('../Service/RedisService.js');
 
-//var redisio = redis.redis;
-
 var ConversationService = function(){};
 
 ConversationService.Init = function(){
 
-      //var conversation = new Conversation(config.conversationConfig.conversation);
-      var conversation = new Conversation(config.conversationConfig.conversation);
+      global.conversation = new Conversation(config.conversationConfig.conversation);
 
-      // let getConversationResponse = (message, context) => {
-      //     let payload = {
-      //       workspace_id : config.conversationConfig.workspace_id,
-      //       context : context || {},
-      //       input : message || {}
-      //     }
-      // }
-      //
-      // payload = preProcess(payload);
-      //
-      // return new Promise((resolved, rejected) => {
-      //
-      //
-      // })
       //onsole.log(redis.redis.set("key" , "value"));
       //console.log(redis);
       // redis.redis.set("key", "value");
       // redis.redis.get("key", (err, result) => { console.log(result); });
 
       redis.redis.set("key", "value");
-      ConversationService.SendMessage("key1", "text", "hello");
+      ConversationService.SendMessage("key", "text", "hello");
 }
+
+// var sendMessage = new Promise((resolve, reject) =>
+//     conversation.message(payload, function(err, data) {
+//       if (err) {
+//         reject(err);
+//       }else{
+//         resolve(data);
+//       }
+//     })
+// );
 
 ConversationService.SendMessage = (useKey, messageType, messageContent) => {
 
@@ -43,44 +36,57 @@ ConversationService.SendMessage = (useKey, messageType, messageContent) => {
     let type = messageType;
     let content = { 'text' : messageContent };
 
+    // let payload = {
+    //
+    //   workspace_id : config.conversationConfig.workspace_id,
+    // }
+
     //console.log(redis.redis.get(key, (error, result) => { console.log(result); }));
 
     redis.redis.get(key).then((result) => {
 
-        //TODO key 값이 있는 경우
-        console.log(result);
+        ConversationService.getConversationResponse({}, content);
+    });
+}
 
-    }).catch((result === nul) {
+ConversationService.getConversationResponse = function(context, message){
 
-        //TODO key 값이 없는 경우
-        console.log("catch in");
+    let payload = {
+
+        workspace_id : config.conversationConfig.workspace_id,
+        context : context || {},
+        input : message || {}
+    }
+
+    var sendMessage = new Promise( (resolve, reject) => {
+
+        conversation.message(payload, (error, data) => {
+
+            if (error) {
+
+              reject(error);
+
+            }else{
+
+              resolve(data);
+            }
+        });
     });
 
-    // var payload = {
-    //
-    //     workspace_id : config.conversationConfig.workspace_id,
-    //     context : context || {},
-    //     input : message || {}
-    // }
+    sendMessage.then(response => {
 
-    // var requestOptions = {
-    //
-    //     method : "GET",
-    //     url :
-    // }
+        //console.log(response);
+        //console.log(response.output);
+        // console.log(response.output.log_messages);
+        var output = response.output.text[0]; // output.text는 배열
+        var new_context = response.context; // wcs 응답 context
+        var message = { "message" : { "text" : output } }
 
+    })
+    .catch(error => {
+
+        console.log(error);
+    });
 }
-
-
-ConversationService.preProcess = function(payload){
-
-    var inputText = payload.input.text;
-    console.log("User Input : " + inputText);
-    console.log("Processed Input : " + inputText);
-    console.log("--------------------------------------------------");
-
-    return payload;
-}
-
 
 module.exports = ConversationService;
